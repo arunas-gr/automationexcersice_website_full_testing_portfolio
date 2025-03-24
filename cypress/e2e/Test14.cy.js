089f2b147adabe44b9b3707e86b98858662bafc5
arunas-gr <arunas.grazulis@gmail.com>
2025-03-24 06:56:15 +0200

    Added Cypress test cases for automation exercise website

--- cypress/e2e/Test14.cy.js
+++ cypress/e2e/Test14.cy.js
@@ -29,11 +29,11 @@ describe('Test Case 14: Place Order: Register while Checkout', () => {
         // 4. Add products to cart
         cy.addProducts();
         cy.url().should('eq', 'https://automationexercise.com/view_cart');
-        
+
         // 5. Click 'Cart' button
         // 6. Verify that cart page is displayed
         cy.get('.breadcrumbs li').contains('Cart').should('be.visible');
-        
+
 
         // 7. Click Proceed To Checkout
         cy.get('.btn').contains('Proceed To Checkout').click();
@@ -53,7 +53,7 @@ describe('Test Case 14: Place Order: Register while Checkout', () => {
         cy.createAccount('VartotojasX', 'vartotojasX@kazkur.com', '123456789');
         cy.get('.shop-menu > .nav > :nth-child(3) > a').contains(' Cart').click();
 
-               // // 11. Verify ' Logged in as username' at top
+        // // 11. Verify ' Logged in as username' at top
         cy.contains('Logged in as VartotojasX').should('exist');
         cy.contains('Cart').click();
 
@@ -67,7 +67,7 @@ describe('Test Case 14: Place Order: Register while Checkout', () => {
         // // 15. Enter description in comment text area and click 'Place Order'
         cy.get('.form-group textarea').type('send my order ASAP');
 
-        
+
         // // 17. Click 'Pay and Confirm Order' button
         cy.get('a[href="/payment"]').contains('Place Order').click();
 
@@ -75,9 +75,9 @@ describe('Test Case 14: Place Order: Register while Checkout', () => {
 
         // // 18. Verify success message 'Your order has been placed successfully!'
         cy.get('h2.title.text-center[data-qa="order-placed"]')
-        .should('be.visible')
-        .and('contain.text', 'Order Placed!');
-        
+            .should('be.visible')
+            .and('contain.text', 'Order Placed!');
+
         // 19. Click 'Delete Account' button
         cy.get('.shop-menu > .nav > :nth-child(5) > a').contains(' Delete Account').click();
 
